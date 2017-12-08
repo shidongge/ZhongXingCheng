03cb708bcca33e6ce3823b9b47cb3a374a8e877b
shido <https://github.com/shidongge/APPdajian>
2017-12-08 17:30:32 +0800

    HAHA

--- app/src/main/java/us/mifeng/zhongxingcheng/activity/CZZX.java
+++ app/src/main/java/us/mifeng/zhongxingcheng/activity/CZZX.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import us.mifeng.zhongxingcheng.R;
 
@@ -18,10 +19,12 @@ import us.mifeng.zhongxingcheng.R;
  * 充值中心
  */
 public class CZZX extends Activity implements View.OnClickListener {
-    private ImageView you;
-    private LinearLayout fangshi,yincang;
+    private ImageView you, wxzf, kqzf;
+    private LinearLayout fangshi, yincang, kuaiqian, weixin;
     private EditText jine;
-    private boolean hah = false;
+    private boolean hah = true;
+    private TextView text;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +33,18 @@ public class CZZX extends Activity implements View.OnClickListener {
     }
 
     private void initView() {
+        text = (TextView) findViewById(R.id.czgl_zffs_text);
         you = (ImageView) findViewById(R.id.czgl_zffs_you);
+        wxzf = (ImageView) findViewById(R.id.czgl_wxzf);
+        kqzf = (ImageView) findViewById(R.id.czgl_kqzf);
         fangshi = (LinearLayout) findViewById(R.id.czgl_fangshi);
         jine = (EditText) findViewById(R.id.czgl_jine);
         yincang = (LinearLayout) findViewById(R.id.czgl_yincang);
-
+        kuaiqian = (LinearLayout) findViewById(R.id.czgl_ll_kuaiqian);
+        weixin = (LinearLayout) findViewById(R.id.czgl_ll_weixin);
         fangshi.setOnClickListener(this);
+        weixin.setOnClickListener(this);
+        kuaiqian.setOnClickListener(this);
 
     }
 
@@ -43,13 +52,22 @@ public class CZZX extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.czgl_fangshi:
-                if (hah=false){
+                if (hah == false) {
                     yincang.setVisibility(View.GONE);
-                    hah=true;
-                }else {
+                    hah = true;
+                } else {
                     yincang.setVisibility(View.VISIBLE);
-                    hah=false;
+                    hah = false;
                 }
+                break;
+            case R.id.czgl_ll_kuaiqian:
+                text.setText("快钱支付");
+
+                yincang.setVisibility(View.GONE);
+                break;
+            case R.id.czgl_ll_weixin:
+                yincang.setVisibility(View.GONE);
+                text.setText("微信支付");
                 break;
             default:
                 break;
